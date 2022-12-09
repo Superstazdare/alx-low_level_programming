1f79fc7b3561215eebfe1d7a279e587af900d009
Superstazdare <superstaz.dare@gmail.com>
2022-12-09 14:04:29 -0800

    print comb

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,11 +17,12 @@ int main(void)
 		{
 			if (j > i)
 			{
-				putchar((i / 10) + '0');
-				putchar((i % 10) + '0');
+				putchar(i / 10 + '0');
+				putchar(i % 10 + '0');
 				putchar(' ');
-				putchar((j / 10) + '0');
-				putchar((j % 10) + '0');
+				putchar(j / 10 + '0');
+				putchar(j % 10 + '0');
+
 				if (i * 100 + j != 9899)
 				{
 					putchar('.');
