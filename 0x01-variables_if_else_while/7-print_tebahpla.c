6ad4e8c5418fb628d41cf322832b5eefeee392a4
Superstazdare <superstaz.dare@gmail.com>
2022-12-09 06:04:47 -0800

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry - point
+ *
+ * Always: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}
