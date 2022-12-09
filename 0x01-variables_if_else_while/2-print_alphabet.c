dd79f3a2c9b5acb55286683f19904187def210c5
Superstazdare <superstaz.dare@gmail.com>
2022-12-09 04:57:54 -0800

    2 Print Alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet.
- *
- * Return: Always 0 (Success)
+ * main - prints alpha in lower case
+ * Return: Always 0
  */
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
+	char lc;
 
-
-	for (i = 0; i < 26; i++)
+	for (lc = 'a'; lc <= 'z'; lc++)
 	{
-		putchar(alp[i]);
+		putchar(lc);
 	}
 	putchar("\n");
-
 	return (0);
 }
