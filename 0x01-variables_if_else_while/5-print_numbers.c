c2c7097f365e56fd33bcc1ad2c476c8eae00a3b6
Superstazdare <superstaz.dare@gmail.com>
2022-12-09 05:28:31 -0800

    5 print numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,14 +4,14 @@
  * Return: Always 0
  */
 
-int main (void)
+int main(void)
 {
 	char c;
 
 	c = 'a';
 	while
 		(c <= 'z') {
-			if (( c != 'q' && c != 'e') && c <= 'z')
+			if ((c != 'q' && c != 'e') && c <= 'z')
 				putchar(c);
 			c++;
 		}
