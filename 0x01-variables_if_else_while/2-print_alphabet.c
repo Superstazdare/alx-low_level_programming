f33e43c8e4a7794a7d7c33491e3beb8b3cfdf392
Superstazdare <superstaz.dare@gmail.com>
2022-12-09 04:46:18 -0800

    2 print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	char alp(26) = "abcdefghijklmnopqrstuvwxyz";
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
 	for (i = 0; i < 26; i++)
 	{
-		putchar(alp(i));
+		putchar(alp[i]);
 	}
 	putchar("\n");
 	return (0);
