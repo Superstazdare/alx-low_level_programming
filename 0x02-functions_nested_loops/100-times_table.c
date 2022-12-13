4bccf65802f92ddb75ad71380cf4a9414bc72293
Superstazdare <superstaz.dare@gmail.com>
2022-12-13 10:12:11 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * print_times_table - Entry point
+ * times_table - Entry point
  * @n: input
  *
  * Return: Always 0 (Success)
  */
 
-void print_times_table(int n)
+void times_table(int n)
 {
 	return (0);
 
