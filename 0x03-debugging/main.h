a46430dd2300e1b9a41ade6e7ec779213c96f396
Superstazdare <superstaz.dare@gmail.com>
2022-12-13 04:21:40 -0800

    main.h

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void postitive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_day(int month, int day, int year);
+int convert_day(int month, int day);
+void print_times_table(int n);
+
+#endif
