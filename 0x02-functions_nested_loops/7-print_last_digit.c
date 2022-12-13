5bc248a9c278006ed9f6f870413af1ab30d6c3c4
Superstazdare <superstaz.dare@gmail.com>
2022-12-13 07:44:56 -0800

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0
- */
+* main - check the code
+*
+* Return: Always 0.
+*/
 
 int main(void)
 {
@@ -13,8 +13,8 @@ int main(void)
 	print_last_digit(98);
 	print_last_digit(0);
 	r = print_last_digit(-1024);
-
 	_putchar('0' + r);
 	_putchar('\n');
+
 	return (0);
 }
