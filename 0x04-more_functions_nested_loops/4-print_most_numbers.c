7c1c2f630da0a3b7cc0b71eed689d335139753c6
Superstazdare <superstaz.dare@gmail.com>
2022-12-15 10:10:50 -0800

    all task

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,19 +1,1 @@
-#include "main.h"
 
-/**
- * print_most_numbers - prints numbers between 0 to 9
- * (neither 2 nor 4).
- * Return: no return.
- */
-
-void print_most_numbers(void)
-{
-	int n;
-
-	for (n = 48; n < 58; n++)
-	{
-		if (n != 50 && n != 52)
-			_putchar(n);
-	}
-	_putchar('\n');
-}
