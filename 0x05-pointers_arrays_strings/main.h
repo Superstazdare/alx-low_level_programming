0686c0f67f7d9d847f380d196940154e576b6ce9
Superstazdare <superstaz.dare@gmail.com>
2022-12-19 02:48:53 -0800

    task 0

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,7 +5,7 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
-vod print_rev(char *s);
+void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
