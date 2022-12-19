cda7837c8494030af8c0a71074e5be82b1619c07
Superstazdare <superstaz.dare@gmail.com>
2022-12-19 07:45:24 -0800

    all task

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,29 +8,34 @@
 
 int _atoi(char *s)
 {
-	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
+	int i, d, n, len, f, digit;
 
-	while (*(s + count) != '\0')
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+	while (i < len && f == 0)
 	{
-		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
-			break;
-		
-		if (*(s + count) == '-')
-				pn *= -1;
-
-		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		if (s[i] == '-')
+			++d;
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			if (size > 0)
-				m *= 10;
-			size++;
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
 		}
-		count++;
+		i++;
 	}
-
-	for (i = count - size; i < count; i++)
-	{
-		oi = oi + ((*(s + i) - 48) * m);
-		m /= 10;
-	}
-	return (oi * pn);
+	if (f == 0)
+		return (0);
+	return (n);
 }
--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -17,5 +17,5 @@ void puts2(char *str)
 		else
 			continue;
 	}
-	_purchar('\n');
+	_putchar('\n');
 }
