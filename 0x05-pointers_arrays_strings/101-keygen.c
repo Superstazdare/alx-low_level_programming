9f1c306a59bbc05febe38ed539e6e2e651ece977
Superstazdare <superstaz.dare@gmail.com>
2022-12-19 08:36:41 -0800

    all task

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -14,11 +14,11 @@ int main(void)
 	int pass[100];
 	int i, sum, n;
 
-	sun = 0;
+	sum = 0;
 
 	srand(time(NULL));
 
-	for (i = 0; im < 100; i++)
+	for (i = 0; i < 100; i++)
 	{
 		pass[i] = rand() % 78;
 		sum += (pass[i] + '0');
