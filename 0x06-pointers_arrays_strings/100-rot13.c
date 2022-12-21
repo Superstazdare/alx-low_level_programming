a42a9d44f7e655193f4f053289487001e2cc4c86
Superstazdare <superstaz.dare@gmail.com>
2022-12-21 10:17:44 -0800

    task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -24,10 +24,10 @@ char *rot13(char *str)
 		'Z', 'A', 'B', 'C', 'D', 'E',
 		'F', 'G', 'H', 'I', 'J', 'K',
 		'L', 'M', 'n', 'o', 'p', 'q',
-		'r', 's', 't', 'u', 'w', 'x',
-		'y', 'z', 'a', 'b', 'c', 'd',
-		'e', 'f', 'g', 'h', 'i', 'j',
-		'k', 'l', 'm'};
+		'r', 's', 't', 'u', 'v', 'w',
+		'x', 'y', 'z', 'a', 'b', 'c',
+		'd', 'e', 'f', 'g', 'h', 'i',
+		'j', 'k', 'l', 'm'};
 	while (str[indx1])
 	{
 		for (indx2 = 0; indx2 < 52; indx2++)
