099047c29287aaf7d02e72ad7537f44f657c18db
Superstazdare <superstaz.dare@gmail.com>
2022-12-21 10:14:35 -0800

    task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,8 +2,9 @@
 
 /**
  * rot13 - Encodes a string using rot13
- * @str: pointer to string params
- * Return: *str
+ * @str: The string to be encoded
+ *
+ * Return: A pointer to be encoded string
  */
 
 char *rot13(char *str)
@@ -17,16 +18,16 @@ char *rot13(char *str)
 		'e', 'f', 'g', 'h', 'i', 'j',
 		'k', 'l', 'm', 'n', 'o', 'p',
 		'q', 'r', 's', 't', 'u', 'v',
-		'w', 'x', 'y, 'z'};
+		'w', 'x', 'y', 'z'};
 	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
 		'T', 'U', 'V', 'W', 'X', 'Y',
 		'Z', 'A', 'B', 'C', 'D', 'E',
 		'F', 'G', 'H', 'I', 'J', 'K',
 		'L', 'M', 'n', 'o', 'p', 'q',
-		'r', 's', 't', 'u', 'w',
-		'x', 'y', 'z', 'a', 'b', 'c',
-		'd', 'e', 'f', 'g', 'h', 'i',
-		'j', 'k', 'l', 'm'};
+		'r', 's', 't', 'u', 'w', 'x',
+		'y', 'z', 'a', 'b', 'c', 'd',
+		'e', 'f', 'g', 'h', 'i', 'j',
+		'k', 'l', 'm'};
 	while (str[indx1])
 	{
 		for (indx2 = 0; indx2 < 52; indx2++)
