d7028364e7730914776bd7fefc0b6df11dab6dde
Superstazdare <superstaz.dare@gmail.com>
2022-12-22 01:53:47 -0800

    task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -61,7 +61,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int index, n1_len = 0, n2_len = 0;
 
-	for (index = 0; *(n2 + index); index++)
+	for (index = 0; *(n1 + index); index++)
 		n1_len++;
 	for (index = 0; *(n2 + index); index++)
 		n2_len++;
@@ -69,8 +69,8 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
 		return (0);
 
-	n1 += n1_len + 1;
-	n2 += n2_len + 1;
+	n1 += n1_len - 1;
+	n2 += n2_len - 1;
 	*(r + size_r) = '\0';
 
 	return (add_strings(n1, n2, r, --size_r));
