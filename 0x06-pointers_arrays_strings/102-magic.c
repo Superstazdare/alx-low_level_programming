4d629cd889291df70c5c437e63fa3edf38e86816
Superstazdare <superstaz.dare@gmail.com>
2022-12-22 01:40:47 -0800

    task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -13,8 +13,11 @@ int main(void)
  * Remember:
  * - you are not allowed to use a
  *   - you are not allowed to modify p
- *   only one statement
- *   you are not allowed to code anything else than this line of code
+ *   -only one statement
+ *   -you are not allowed to code anything else than this line of code
  */
-	p[5]
+	*(p + 5) = 98;
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2];
+			return (0);
 }
