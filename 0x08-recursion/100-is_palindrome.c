26e6324117ffe422f85755543533d973a03704b3
Superstazdare <superstaz.dare@gmail.com>
2023-01-04 09:58:30 -0800

    recursion task

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -22,3 +22,15 @@ int find_strlen(char *s)
 
 	return (len);
 }
+/**
+ * is_palindrome - Checks if a string is a palindrome
+ * @s: string to reverse
+ *
+ * Return: 1 if it is, 0 it's not
+ */
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
+}
