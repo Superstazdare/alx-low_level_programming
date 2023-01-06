b0c6a74de4606c5265e326fa935cf4a81623bb73
Superstazdare <superstaz.dare@gmail.com>
2023-01-06 09:25:28 -0800

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,15 +12,10 @@ int _atoi(char *s)
 	int i, d, n, len, f, digit;
 
 	i = 0;
-	
 	d = 0;
-
 	n = 0;
-
 	len = 0;
-
 	f = 0;
-
 	digit = 0;
 
 	while (s[len] != '\0')
@@ -32,6 +27,7 @@ int _atoi(char *s)
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
+
 			if (d % 2)
 				digit = -digit;
 			n = n * 10 + digit;
