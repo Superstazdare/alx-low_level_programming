88070775c049c53b317e91f8c2f8f475d6c6fb0a
Olowookun Oluwadare Sunday <117734729+Superstazdare@users.noreply.github.com>
2023-01-06 19:17:36 +0100

    Update 0-memset.c

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
- * _memset - fill a block of memory with a specific value
- * @s: starting address of memory to be filled
- * @b: the desired value
- * @n: number of bytes to be changed
+ * _memset - This is my function copy the number the character to S
+ * @s: This is my entry and return
+ * @b: This is the character to copy
+ * @n: This is the number of Bytes
  *
- * Return: changed array with new value for n bytes
+ * Return: This is my result and Return S
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0;
+	unsigned int a;
 
-	for (; n > 0; i++)
+	for (a = 0; a < n ; a++)
 	{
-		s[i] = b;
-		n--;
+		s[a] = b;
 	}
 	return (s);
 }
