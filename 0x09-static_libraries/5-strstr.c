8581a66c6ef98cd78a40ade30c8301fc93718ba9
Olowookun Oluwadare Sunday <117734729+Superstazdare@users.noreply.github.com>
2023-01-06 19:32:13 +0100

    Update 5-strstr.c

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,27 +1,34 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _strstr - Entry point
- * @haystack: input
- * @needle: input
+ * _strstr - locates a substring inside a string
  *
- * Return: Always 0 (Success)
+ * @haystack: string to search
+ * @needle: substring to search for
+ * Return: returns pointer to location of substring inside haystack or NULL
  */
 
 char *_strstr(char *haystack, char *needle)
 {
-	for (; *haystack != '\0'; haystack++)
-	{
-		char *l = haystack;
-		char *p = needle;
+	int i;
 
-		while (*l == *p && *p != '\0')
+	i = 0;
+	while (haystack[i] != '\0')
+	{
+		int j, start;
+
+		start = i;
+		j = 0;
+		while (haystack[i] == needle[j] &&
+		       needle[j] != '\0' && haystack[i] != '\0')
 		{
-			l++;
-			p++;
+			i++;
+			j++;
 		}
-		if (*p == '\0')
-			return (haystack);
+		if (needle[j] == '\0')
+			return (haystack + start);
+		i = start + 1;
 	}
-	return (0);
+	return (NULL);
 }
