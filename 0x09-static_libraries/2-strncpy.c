47ffa025eb7f338b688594929b0a9951993337ac
Olowookun Oluwadare Sunday <117734729+Superstazdare@users.noreply.github.com>
2023-01-06 19:24:18 +0100

    Update 2-strncpy.c

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,27 +1,24 @@
 #include "main.h"
-
 /**
- * _strncpy - copy a string
- * @dest: input value
- * @src: input value
- * @n: input value
+ *  _strncpy - This is my main
+ *  @dest: This is my dest
+ *  @src: This is my source
+ *  @n: This is my N
  *
- * Return: dest
+ *  Return: THis is my return
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
-	int j;
+	int b;
 
-	j = 0;
-	while (j < n && src[j] != '\0')
+	for (b = 0; b < n && src[b] != '\0' ; b++)
 	{
-		dest[j] = src[j];
-		j++;
+	dest[b] = src[b];
 	}
-	while (j < n)
+	for (; b  < n; b++)
 	{
-		dest[j] = '\0';
-		j++;
+		dest[b] = '\0';
 	}
 	return (dest);
 }
