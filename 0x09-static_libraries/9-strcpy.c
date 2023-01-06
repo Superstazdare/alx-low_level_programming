2fd1836274be1c7cfeae7c5fb9d765261c0ded3b
Olowookun Oluwadare Sunday <117734729+Superstazdare@users.noreply.github.com>
2023-01-06 18:57:57 +0100

    Update 9-strcpy.c

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,24 +1,22 @@
 #include "main.h"
 
 /**
- * char *_strcpy - a function that copies the string pointed to by src
- * @dest: copy to
- * @src: copy from
- * Return: string
+ * _strcpy - Copiar el contenido de uno a otra variable
+ *  @dest: This is destiny
+ *  @src: This is the copia
+ *
+ *  Return: This return copy
  */
 char *_strcpy(char *dest, char *src)
 {
-	int l = 0;
-	int x = 0;
+	char *start = dest;
 
-	while (*(src + l) != '\0')
+	while (*src != '\0')
 	{
-		l++;
+		*dest = *src;
+		dest++;
+		src++;
 	}
-	for (; x < l; x++)
-	{
-		dest[x] = src[x];
-	}
-	dest[l] = '\0';
-	return (dest);
+	*dest = '\0';
+	return (start);
 }
