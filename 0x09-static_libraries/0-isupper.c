8bf461669c8c54e748c685da703d3a588ba7f1d8
Olowookun Oluwadare Sunday <117734729+Superstazdare@users.noreply.github.com>
2023-01-06 19:15:59 +0100

    Update 0-isupper.c

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,15 +1,21 @@
 #include "main.h"
 
 /**
- * _isupper - uppercase letters
- * @c: char to char
+ * _isupper - If is uppercase return 1, if not return 0
  *
- * Return: 0 or 1
- */
+ *  @c: this is the entry
+ *
+ *  Return: 0
+ *
+ **/
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
