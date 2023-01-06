3bb217e3464263413447bef7eff231a4e0a011b6
Olowookun Oluwadare Sunday <117734729+Superstazdare@users.noreply.github.com>
2023-01-06 19:27:46 +0100

    Update 4-isalpha.c

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,11 +1,20 @@
 #include "main.h"
 
 /**
- * _isalpha - checks for alphabetic character
- * @c: the character to be checked
- * Return: 1 if c is a letter, 0 otherwise
+ *_isalpha - check the the entry to deteminate if is lower or upper
+ *
+ * @c: This is the entry
+ * Return: Always 0.
  */
+
 int _isalpha(int c)
 {
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
