874a23f167d4dbfff917d51de8631a14a4458274
Superstazdare <superstaz.dare@gmail.com>
2023-01-06 04:58:26 -0800

    static task

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+void _puts(char *str);
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
