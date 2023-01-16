3fa1bcbbe0c0f02225553c28eb46a8ce6b3e9666
Superstazdare <superstaz.dare@gmail.com>
2023-01-16 04:06:53 -0800

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -60,7 +60,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (doggo->owner == NULL)
 	{
 		free(doggo);
-		return(NULL);
+		return (NULL);
 	}
 	doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
 	if (doggo->owner == NULL)
