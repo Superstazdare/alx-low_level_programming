6936e48a8a5ebe33679fd55d521bf2fc54f313e0
Superstazdare <superstaz.dare@gmail.com>
2023-01-16 04:11:50 -0800

    task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * init_dog - Initializes a variable of type struct dog.
+ * init_dog - initializes a variable of type struct dog.
  * @d: pointer to struct dog to initialize.
  * @name: name to initialize
  * @age: age to initialize.
