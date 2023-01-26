2e6ad0329c4219817f192f24d559599a8fee8b55
Superstazdare <superstaz.dare@gmail.com>
2023-01-26 01:17:02 -0800

    list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,7 +5,7 @@
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
- * @next: points to the next node 
+ * @next: points to the next node
  *
  * Description: singly linked list node structure
  * for Holberton project
