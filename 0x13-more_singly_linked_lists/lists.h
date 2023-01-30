6f306f867a3b8fb95e8987bf208fc7e1fc105176
Superstazdare <superstaz.dare@gmail.com>
2023-01-30 10:31:15 -0800

    more singly update

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,7 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -10,12 +9,13 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for alx project
+ * for project
  */
+
 typedef struct listint_s
 {
-        int n;
-        struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -34,4 +34,4 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
-#endif
+#endif /* LISTS_H */
