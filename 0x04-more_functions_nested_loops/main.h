de1a07460d853e5a41d3015eab07813805102323
Superstazdare <superstaz.dare@gmail.com>
2023-02-01 07:15:07 -0800

    update

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,11 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
 void print_numbers(void);
+void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
