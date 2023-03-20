b0c4ee40e740f5bef2d0e42f46e7579fdd9d8e39
Superstazdare <superstaz.dare@gmail.com>
2023-03-20 09:30:17 -0700

    dynamic task

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Superstazdare/alx-low_level_programming/0x18-dynamic_libraries/giga.so
-export LD_PRELOAD=/tmp/giga.so
+wget -P .. https://raw.githubusercontent.com/monoprosito/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
